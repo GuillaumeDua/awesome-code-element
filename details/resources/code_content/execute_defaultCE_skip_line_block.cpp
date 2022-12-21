ae40c90240e76c994eee90c957254fdadc9ddc6d
GuillaumeDua <pro.guillaume.dua@gmail.com>
2022-12-21 15:06:47 +0000

    Deploying to gh-pages from @ GuillaumeDua/awesome-code-element@45269de03117b910621fa5a73418d1963109b7d9 🚀

---
+++ details/resources/code_content/execute_defaultCE_skip_line_block.cpp
@@ -0,0 +1,19 @@
+// some comment here
+// ...
+
+// @awesome-doc-code-sections::CE={
+//  "add_in_doc_execution" : true
+//  }
+
+
+auto main() -> int {
+    return 42;
+}
+
+BAD CODE // @awesome-doc-code-sections::skip::line 
+
+// @awesome-doc-code-sections::skip::block::begin
+// skip me !
+// and me !
+// and me too !
+// @awesome-doc-code-sections::skip::block::end
