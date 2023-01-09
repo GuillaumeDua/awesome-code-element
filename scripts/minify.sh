cfdfd2a356b02c39c1126bcbcb2502f8936ba1d9
GuillaumeDua <guillaume.dua@gmail.com>
2023-01-09 21:52:25 +0100

    [CI] automated release deployement

--- scripts/minify.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# sudo apt install -y npm && sudo npm install -y uglify-js -g && sudo npm install -y uglifycss -g
-
-uglifyjs --compress --mangle --module -- awesome-code-element.js > awesome-code-element.min.js
-uglifycss styles/default.css > styles/default.min.css
