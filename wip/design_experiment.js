d260efbad590cf68e31dc8cb8463435baa706446
GuillaumeDua <guillaume.dua@gmail.com>
2023-02-08 23:26:18 +0100

    [JS] #34:CS: cs.details.code: integration (WIP): experimenting composition on

--- wip/design_experiment.js
+++ wip/design_experiment.js
@@ -1,4 +1,4 @@
-const op_bundle_1 = (state) => ({
+const feature_value_storage = (state) => ({
     op_1: () => console.log('op_1'),
     get value() {
         return state.details.value
@@ -8,11 +8,10 @@ const op_bundle_1 = (state) => ({
         state.NotifyPropertyChanged({ property_name: 'value' })
     }
 })
-const op_bundle_NotifyPropertyChangedInterface = (state, parameters) => {
-    
-    handlers = new Map // private storage // TODO: as member
+const feature_NotifyPropertyChangedInterface = (state, parameters) => {
 
     return {
+        _NotifyPropertyChangedInterface_handlers: new Map,
         _constructor(){
 
             if (!parameters)
@@ -23,19 +22,19 @@ const op_bundle_NotifyPropertyChangedInterface = (state, parameters) => {
             parameters.forEach((value, index) => {
                 if (!(value instanceof Array) || value.length !== 2)
                     throw new Error(`NotifyPropertyChangedInterface.constructor: invalid argument (at index ${index})`)
-                state.add_OnPropertyChangeHandler({ property_name: value[0], handler: value[1] })
+                this.add_OnPropertyChangeHandler({ property_name: value[0], handler: value[1] })
             })
         },
         add_OnPropertyChangeHandler({property_name, handler}) {
             if (!(handler instanceof Function))
                 throw new Error('NotifyPropertyChangedInterface.add_OnPropertyChangeHandler: invalid argument')
-            handlers.set(property_name, handler)
+            this._NotifyPropertyChangedInterface_handlers.set(property_name, handler)
         },
         remove_OnPropertyChangeHandler({property_name}) {
-            handlers.delete(property_name)
+            this._NotifyPropertyChangedInterface_handlers.delete(property_name)
         },
         NotifyPropertyChanged({property_name}){
-            const handler = handlers.get(property_name)
+            const handler = this._NotifyPropertyChangedInterface_handlers.get(property_name)
             if (handler)
                 handler({
                     property_name: property_name,
@@ -46,6 +45,17 @@ const op_bundle_NotifyPropertyChangedInterface = (state, parameters) => {
 }
 
 class composition_factory {
+
+    static is_class_definition(value){
+        return value.prototype
+            && value.prototype.constructor.toString
+            && value.prototype.constructor.toString().substring(0, 5) === 'class'
+    }
+    static is_class_value(value){
+        return value.constructor.toString
+            && value.constructor.toString().substring(0, 5) === 'class'
+    }
+
     static is_feature_descriptor = (value) => {
         return value && value.value && value.parameters
     }
@@ -56,7 +66,9 @@ class composition_factory {
          || features.filter(feature => !feature).length)
             throw new Error('composition_factory: invalid argument')
 
-        state = structuredClone(state) // explicit copy
+        state = composition_factory.is_class_definition(state)
+            ? new state
+            : structuredClone(state)
 
         features.map(feature => {
             if (!composition_factory.is_feature_descriptor(feature)) {
@@ -76,13 +88,52 @@ class composition_factory {
         
         return state
     }
-    static make_composition = ({ state, features }) => {
-        // return () => composition_factory.make_prototype({ state: state, features: features })
-        return class {
-            constructor(){
-                const prototype = composition_factory.make_prototype({ state: state, features: features })
-                Object.defineProperties(this, Object.getOwnPropertyDescriptors(prototype))
+    static make_composition = ({ state, features, extends_type = undefined }) => {
+        return extends_type
+            ? class extends extends_type {
+                constructor(){
+                    super()
+                    const prototype = composition_factory.make_prototype({ state: state, features: features })
+                    Object.defineProperties(this, Object.getOwnPropertyDescriptors(prototype))
+                }
             }
-        }
+            : class {
+                constructor(){
+                    const prototype = composition_factory.make_prototype({ state: state, features: features })
+                    Object.defineProperties(this, Object.getOwnPropertyDescriptors(prototype))
+                }
+            }
     }
 }
+
+// ---
+
+class state {
+    details = { value: 42 }
+}
+const type_1 = composition_factory.make_composition({
+    state: state,
+    features: [
+        feature_value_storage,
+        {
+            value: feature_NotifyPropertyChangedInterface,
+            parameters: [
+                [
+                    [
+                        'value', (arg) => console.log('value changer to:', arg)
+                    ]
+                ]
+            ]
+        }
+    ],
+    extends_type: HTMLElement
+})
+customElements.define('type-1', type_1)
+
+// ---
+
+document.addEventListener("DOMContentLoaded", () => {
+    value = new type_1
+    document.body.prepend(value)
+    value.innerHTML = '===> Hello, there <==='
+});
