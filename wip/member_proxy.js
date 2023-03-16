723eda41abe7ff823fe63c9c6dd3c72b0bac6808
GuillaumeDua <guillaume.dua@gmail.com>
2023-03-16 13:39:17 +0100

    [JS] attr sync poc

--- wip/member_proxy.js
+++ wip/member_proxy.js
@@ -11,52 +11,77 @@ class on_property_changed {
         if (!descriptor.configurable)
             throw new Error(`inject_property_proxy: [${property_name}] is not configurable`)
 
-        console.debug('>>> debug descriptor', property_name, descriptor)
-
         let storage = undefined
         const payload = (value) => {
             if (value !== storage){
+                console.log('proxy for [', property_name, '] with value = [', value, '](', typeof value, ') with storage=[', storage, ']')
                 storage = value
-                console.log('proxy for [', property_name, '] with value = [', value, '](', typeof value, ')')
                 callback(value)
             }
         }
 
-        const getter = (() => {
-            if (descriptor.get)
-                return function(){
-                    const value = descriptor.get.call(this);
-                    payload(value);
-                    return value
-                }
-            if (descriptor.value)
-                return function(){ payload(descriptor.value); return descriptor.value }
-            return undefined
-        })()
-        const setter = (() => {
-            if (descriptor.set)
-                return function(value){
-                    descriptor.set.call(this, value);
-                    payload(descriptor.get ? descriptor.get.call(this) : value)
-                }
-            if (descriptor.value)
-                return function(value){
-                    descriptor.value = value
-                    payload(value);
-                }
-            return undefined
-        })()
+        const bypass = {
+            getter: (() => {
+                if (descriptor.get)
+                    return function(){ 
+                        const value = descriptor.get.call(target);
+                        storage = value
+                        return value
+                    }
+                if (descriptor.value)
+                    return function(){ return storage = descriptor.value }
+                return undefined
+            })(),
+            setter: (() => {
+                if (descriptor.set)
+                    return function(value){
+                        descriptor.set.call(target, value);
+                        storage = descriptor.get ? descriptor.get.call(this) : value
+                    }
+                if (descriptor.value)
+                    return function(value){
+                        descriptor.value = value
+                        storage = value
+                    }
+                return undefined
+            })()
+        }
 
-        Object.defineProperty(target, property_name, {
-            get: getter,
-            set: setter
-        });
+        const new_descriptor = {
+            get: (() => {
+                if (descriptor.get)
+                    return function(){
+                        const value = descriptor.get.call(this);
+                        payload(value);
+                        return value
+                    }
+                if (descriptor.value)
+                    return function(){ payload(descriptor.value); return descriptor.value }
+                return undefined
+            })(),
+            set: (() => {
+                if (descriptor.set)
+                    return function(value){
+                        descriptor.set.call(this, value);
+                        payload(descriptor.get ? descriptor.get.call(this) : value)
+                    }
+                if (descriptor.value)
+                    return function(value){
+                        descriptor.value = value
+                        payload(value);
+                    }
+                return undefined
+            })()
+        }
+
+        Object.defineProperty(target, property_name, new_descriptor);
 
         // spread initiale value, if any
         target[property_name]
 
         return {
             revoke: () => { Object.defineProperty(target, property_name, descriptor) },
+            bypass: bypass,
             initiale_descriptor: descriptor
         }
     }
